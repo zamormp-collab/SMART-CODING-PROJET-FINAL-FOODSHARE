265787c194dca195cfa841249f2fb531f3739fcf
Patrick Delva <delvapatrick25@gmail.com>
2026-05-16 14:03:29 -0400

    Feat: Design UI de la page RegisterScreen pas encore de logique metier avec API

--- app/src/main/java/com/example/foodshare/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/foodshare/ui/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.foodshare.ui.screens.auth.LoginScreen
+import com.example.foodshare.ui.screens.auth.RegisterScreen
 import com.example.foodshare.viewmodel.AuthViewModel
 
 @Composable
@@ -17,9 +18,25 @@ fun NavGraph(navController: NavHostController, authViewModel: AuthViewModel) {
             LoginScreen(
                 viewModel = authViewModel,
                 onLoginSuccess = {
-                navController.navigate(Screen.Home.route) {
-                    popUpTo(Screen.Login.route) { inclusive = true }
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                },
+                onSignUpClick = {
+                    navController.navigate(Screen.Register.route)
                 }
+            )
+        }
+
+        composable(Screen.Register.route) {
+            RegisterScreen(
+                onRegisterSuccess = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.Register.route) { inclusive = true }
+                    }
+                },
+                onLoginClick = {
+                    navController.popBackStack()
                 }
             )
         }

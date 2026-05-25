baf38f8c78465473916d766bb12efc61d34a4c6a
Patrick Delva <delvapatrick25@gmail.com>
2026-05-24 23:20:53 -0400

    Corrige import verticalScroll dans HomeScreen

--- app/src/main/java/com/example/foodshare/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/foodshare/ui/screens/home/HomeScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.verticalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons

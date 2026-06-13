254a4a54978d70579329d128f33d9199a3dd20de
Marc Philippe ZAMOR <zamormp@gmail.com>
2026-06-13 06:55:52 -0400

    fix(api): add StatutOffre enum (restore missing enum used by Offre)

---
+++ api/src/main/java/ht/edu/ueh/fds/frst/cdwm/smartcoding/foodshare/api/enums/StatutOffre.java
@@ -0,0 +1,10 @@
+package ht.edu.ueh.fds.frst.cdwm.smartcoding.foodshare.api.enums;
+
+/**
+ * Statut d'une offre : ACTIVE, EXPIREE, ANNULEE
+ */
+public enum StatutOffre {
+    ACTIVE,
+    EXPIREE,
+    ANNULEE
+}
